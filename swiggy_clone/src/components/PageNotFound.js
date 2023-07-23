4d142eef5add1b74c5d2f40185d492a22ca528fe
Praveen Dsouza <dsouzapraveen24@gmail.com>
2023-07-23 19:01:39 +0530

    route home on button click

--- swiggy_clone/src/components/PageNotFound.js
+++ swiggy_clone/src/components/PageNotFound.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { PAGE_NOT_FOUND_URL } from "../utils/constants";
 
 const PageNotFound = () => {
@@ -5,7 +6,11 @@ const PageNotFound = () => {
     <div className="font-default flex items-center justify-center leading-5 min-h-[calc(100vh-229px)]">
       <div className="text-center w-[430px] py-[120px]">
         <div className="bg-contain bg-no-repeat bg-[50%]">
-          <img className="h-[260px] inline-block" src={PAGE_NOT_FOUND_URL} alt="pagenotfound_img" />
+          <img
+            className="h-[260px] inline-block"
+            src={PAGE_NOT_FOUND_URL}
+            alt="pagenotfound_img"
+          />
         </div>
         <div className="text-[#282c3f] text-[32px] mt-[30px] mb-[10px] font-semibold">
           Page not found
@@ -14,7 +19,11 @@ const PageNotFound = () => {
           Uh-oh! Looks like the page you are trying to access, doesn't exist.
           Please start afresh.
         </div>
-        <button className="cursor-pointer text-center bg-[#fc8019] mt-[25px] text-[15px] text-white px-5 h-10 font-semibold leading-10 w-auto inline-block ">GO HOME</button>
+        <Link to="/">
+          <button className="cursor-pointer text-center bg-[#fc8019] mt-[25px] text-[15px] text-white px-5 h-10 font-semibold leading-10 w-auto inline-block ">
+            GO HOME
+          </button>
+        </Link>
       </div>
     </div>
   );
