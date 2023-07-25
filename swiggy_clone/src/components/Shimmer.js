4df2989004339c3d9e0d0e407f4b2f500f1db4c3
Praveen Dsouza <dsouzapraveen24@gmail.com>
2023-07-25 17:07:11 +0530

    image size changes

--- swiggy_clone/src/components/Shimmer.js
+++ swiggy_clone/src/components/Shimmer.js
@@ -3,56 +3,56 @@ const Shimmer = ({ resList }) => {
     <div className="my-8 mx-4">
       <div className="grid flex-wrap p-0 item-center grid-cols-4 gap-8">
         <div className="h-[250px] w-[250px]">
-          <div className="rounded-xl bg-gray-200 h-[120px] w-[180px] mb-3"></div>
+          <div className="rounded-xl bg-gray-200 h-[130px] w-[200px] mb-3"></div>
           <div className="ml-3">
             <div className="bg-gray-200 h-2 w-[140px] my-3"></div>
             <div className="bg-gray-200 h-2 w-[100px]"></div>
           </div>
         </div>
         <div className="h-[250px] w-[250px]">
-          <div className="rounded-xl bg-gray-200 h-[120px] w-[180px] mb-3"></div>
+          <div className="rounded-xl bg-gray-200 h-[130px] w-[200px] mb-3"></div>
           <div className="ml-3">
             <div className="bg-gray-200 h-2 w-[140px] my-3"></div>
             <div className="bg-gray-200 h-2 w-[100px]"></div>
           </div>
         </div>
         <div className="h-[250px] w-[250px]">
-          <div className="rounded-xl bg-gray-200 h-[120px] w-[180px] mb-3"></div>
+          <div className="rounded-xl bg-gray-200 h-[130px] w-[200px] mb-3"></div>
           <div className="ml-3">
             <div className="bg-gray-200 h-2 w-[140px] my-3"></div>
             <div className="bg-gray-200 h-2 w-[100px]"></div>
           </div>
         </div>
         <div className="h-[250px] w-[250px]">
-          <div className="rounded-xl bg-gray-200 h-[120px] w-[180px] mb-3"></div>
+          <div className="rounded-xl bg-gray-200 h-[130px] w-[200px] mb-3"></div>
           <div className="ml-3">
             <div className="bg-gray-200 h-2 w-[140px] my-3"></div>
             <div className="bg-gray-200 h-2 w-[100px]"></div>
           </div>
         </div>
         <div className="h-[250px] w-[250px]">
-          <div className="rounded-xl bg-gray-200 h-[120px] w-[180px] mb-3"></div>
+          <div className="rounded-xl bg-gray-200 h-[130px] w-[200px] mb-3"></div>
           <div className="ml-3">
             <div className="bg-gray-200 h-2 w-[140px] my-3"></div>
             <div className="bg-gray-200 h-2 w-[100px]"></div>
           </div>
         </div>
         <div className="h-[250px] w-[250px]">
-          <div className="rounded-xl bg-gray-200 h-[120px] w-[180px] mb-3"></div>
+          <div className="rounded-xl bg-gray-200 h-[130px] w-[200px] mb-3"></div>
           <div className="ml-3">
             <div className="bg-gray-200 h-2 w-[140px] my-3"></div>
             <div className="bg-gray-200 h-2 w-[100px]"></div>
           </div>
         </div>
         <div className="h-[250px] w-[250px]">
-          <div className="rounded-xl bg-gray-200 h-[120px] w-[180px] mb-3"></div>
+          <div className="rounded-xl bg-gray-200 h-[130px] w-[200px] mb-3"></div>
           <div className="ml-3">
             <div className="bg-gray-200 h-2 w-[140px] my-3"></div>
             <div className="bg-gray-200 h-2 w-[100px]"></div>
           </div>
         </div>
         <div className="h-[250px] w-[250px]">
-          <div className="rounded-xl bg-gray-200 h-[120px] w-[180px] mb-3"></div>
+          <div className="rounded-xl bg-gray-200 h-[130px] w-[200px] mb-3"></div>
           <div className="ml-3">
             <div className="bg-gray-200 h-2 w-[140px] my-3"></div>
             <div className="bg-gray-200 h-2 w-[100px]"></div>
