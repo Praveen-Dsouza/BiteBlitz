3b337b3e20d608c2df2ce6b84338dc0a5a643f5f
Praveen Dsouza <dsouzapraveen24@gmail.com>
2023-07-29 14:18:35 +0530

    added restaurant category accordian header

---
+++ swiggy_clone/src/components/restaurant/restaurantMenu/ItemList.js
@@ -0,0 +1,5 @@
+const ItemList = ({items}) => {
+    return <div>Category</div>
+}
+
+export default ItemList;
--- swiggy_clone/src/components/restaurant/restaurantMenu/RestaurantMenu.js
+++ swiggy_clone/src/components/restaurant/restaurantMenu/RestaurantMenu.js
@@ -1,47 +1,36 @@
 import Shimmer from "../../Shimmer";
 import { useParams } from "react-router-dom";
 import useRestaurantMenu from "../../../utils/hooks/useRestaurantMenu";
-import { useContext } from "react";
-import SearchContext from "../../../utils/SearchContext";
+import RestaurantCategory from "./RestaurantCategory";
 
 const RestaurantMenu = () => {
   const { resId } = useParams();
   const resInfo = useRestaurantMenu(resId);
-  const { searchRestaurant } = useContext(SearchContext)
 
   if (resInfo === null) return <Shimmer />;
 
-  const { name, cuisines, costForTwoMessage } = resInfo?.cards[0]?.card?.card?.info;
-    
-  const { itemCards } = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card
-  // resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card
-  // resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR.cards[3].card.card.categories[0];
+  const { name, cuisines, costForTwoMessage } =
+    resInfo?.cards[0]?.card?.card?.info;
 
-  return resInfo === null? <Shimmer/> :(
+  const categories =
+    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
+      (cat) => 
+        cat?.card?.card?.["@type"] ===
+          "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+      
+    );
+
+  return resInfo === null ? (
+    <Shimmer />
+  ) : (
     <div className="text-center">
       <h1 className="font-bold my-6 text-2xl">{name}</h1>
       <p className="font-bold text-lg">
         {cuisines.join(", ")} - {costForTwoMessage}
       </p>
       <h2 className="font-bold">Menu</h2>
-      <ul>
-
-        {itemCards
-        ?.filter((res) => {
-          if (searchRestaurant === "") {
-            return res;
-          } else {
-            return res?.card?.info?.name.toLowerCase().includes(searchRestaurant);
-          }
-        })
-        ?.map((item) => (
-          <li key={item?.card?.info?.id}>
-            {item?.card?.info?.name} - {" Rs."}
-            {item?.card?.info?.price / 100 ||
-              item?.card?.info?.defaultPrice / 100}
-          </li>
-        ))}
-      </ul>
+      {/* categories accordian */}
+      {categories.map((category) => <RestaurantCategory data={category?.card?.card} />)}
     </div>
   );
 };
