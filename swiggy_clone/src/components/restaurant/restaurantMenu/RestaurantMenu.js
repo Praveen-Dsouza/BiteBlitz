de302f51193d6531b81319901546eaf08e7a86c4
Praveen Dsouza <dsouzapraveen24@gmail.com>
2023-07-30 19:26:59 +0530

    added toggle not working

--- swiggy_clone/src/components/restaurant/restaurantMenu/RestaurantMenu.js
+++ swiggy_clone/src/components/restaurant/restaurantMenu/RestaurantMenu.js
@@ -34,7 +34,8 @@ const RestaurantMenu = () => {
 
   const { offers } = resInfo.cards[1]?.card?.card?.gridElements?.infoWithStyle;
 
-  console.log(offers);
+  const { title } = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card
+console.log(resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.title)
 
   const categories =
     resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
@@ -204,6 +205,26 @@ const RestaurantMenu = () => {
             </div>
           </div>
           {/* Coupon ends */}
+          {/* Veg/Non-Veg option toggle */}
+          <div>
+             <div className="mt-6 mx-4 flex items-center justify-between font-default">
+                  <div className="flex items-center h-[14px] tranform translate-z-0 backface-hidden">
+                    <div className="text-[#3d4152] font-semibold text-[16px] font-default">Veg Only</div>
+                    <button className="ml-3 text-0 p-0 cursor-pointer text-left outline-none">
+                      <span className="h-[18px] backface-hidden bg-[#d4d5d9] w-9 relative tranform translate-z-0 inline-block rounded-[4px] [contain:layout] [transition:background-color 0.25s linear]">
+                        <span className="border-[#00800000] absolute w-[18px] bg-white border-b-[2px] border-[#d4d5d9] rounded-[4px] backface-hidden "></span>
+                      </span>
+                    </button>
+                  </div>
+                  <div className="h-[0.5px] my-6 mx-auto w-[calc(100% - 32px)] border-b-[0.5px] border-[#d3d3d3]"></div>
+             </div>
+          </div>
+          {/* Veg/Non-Veg option toggle Ends */}
+          {/* Menu */}
+          <div className="w-full bg-white">
+            <h2 className="px-4 mb-6 font-default font-bold text-[18px]">{title}</h2>
+          </div>
+          {/* Menu Ends */}
         </div>
       </div>
     </div>
