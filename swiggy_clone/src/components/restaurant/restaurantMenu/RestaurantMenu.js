473f5b1cb42156e56f94554e2dcb08b95b0b707a
Praveen Dsouza <dsouzapraveen24@gmail.com>
2023-07-31 14:21:10 +0530

    added crousel flex

--- swiggy_clone/src/components/restaurant/restaurantMenu/RestaurantMenu.js
+++ swiggy_clone/src/components/restaurant/restaurantMenu/RestaurantMenu.js
@@ -233,7 +233,9 @@ const RestaurantMenu = () => {
               {title}
             </h2>
             <div className="mb-3 overflow-x-auto overflow-y-hidden whitespace-nowrap scroll-pl-[25px] scroll-x-mandatory w-full">
+              <div className="inline-flex">
               {carousel.map((item) => {
+                
                 return (
                   <CarouselItem
                     key={item?.bannerId}
@@ -241,9 +243,10 @@ const RestaurantMenu = () => {
                     title={item?.title}
                     description={item?.dish?.info?.description}
                     price={item?.dish?.info?.price}
-                  />
-                );
-              })}
+                    />
+                    );
+                  })}
+                  </div>
             </div>
           </div>
           {/* Menu Ends */}
